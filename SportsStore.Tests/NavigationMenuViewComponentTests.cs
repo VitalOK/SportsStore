8950334db5573bb41948af7b1e0dc78470a85e6b
Papusa Vitalie <papusav@gmail.com>
2019-06-03 08:15:08 +0300

    pag 265

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Moq;
 using SportsStore.Components;
+using SportsStore.Controllers;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -62,6 +65,39 @@ namespace SportsStore.Tests
 
             //Assert
             Assert.Equal(categoryToSelect, result);
+        }
+
+        [Fact]
+        public void Generate_Category_Specific_Product_Count()
+        {
+            //Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]{
+                new Product {ProductID=1, Name="P1", Category="Cat1"},
+                new Product {ProductID=1, Name="P2", Category="Cat2"},
+                new Product {ProductID=1, Name="P3", Category="Cat1"},
+                new Product {ProductID=1, Name="P4", Category="Cat2"},
+                new Product {ProductID=1, Name="P5", Category="Cat3"}
+            }).AsQueryable<Product>());
+
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            Func<ViewResult, ProductsListViewModel> GetModel = result => result?.ViewData?.Model as ProductsListViewModel;
+
+            //Action
+            int? res1 = GetModel(target.List("Cat1"))?.PagingInfo.TotalItems;
+            int? res2 = GetModel(target.List("Cat2"))?.PagingInfo.TotalItems;
+            int? res3 = GetModel(target.List("Cat3"))?.PagingInfo.TotalItems;
+            int? resAll = GetModel(target.List(null))?.PagingInfo.TotalItems;
+
+            //Assert
+            Assert.Equal(2, res1);
+            Assert.Equal(2, res2);
+            Assert.Equal(1, res3);
+            Assert.Equal(5, resAll);
+
         }
     }
 }
